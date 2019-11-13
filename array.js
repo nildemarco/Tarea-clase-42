15a092ad7b184b2297e01791cfb3455411bdd8db
nildemarco <nildemarco@gmail.com>
2019-11-13 20:28:34 -0300

    ejercicios

---
+++ array.js
@@ -0,0 +1,57 @@
+//Ejercicio 1
+ let playlistFoo = ['Everlong', 'The Pretender', 'Learn to Fly', 'Best of You', 'All My Life', 'Monkey Wrench', 'The Sky is a Neighborhood']
+
+ console.log(playlistFoo[1]);
+ console.log(playlistFoo[4]);
+
+ //Ejercicio 2
+
+ let bandas = ['Maroon 5', 'Queen','No te va a gustar', 'Los cafres', 'Los autenticos decadentes', 'Intoxicados', 'Las pastillas del abuelo','Morat']
+
+ let primerbanda = bandas[0];
+ let tercerbanda = bandas[2];
+ let sextabanda = bandas[5];
+
+ console.log(`La banda en primer lugar es ${primerbanda}`);
+
+ console.log(`La banda en tercer lugar es: ${tercerbanda}`);
+
+ console.log(`La banda en sexto lugar es: ${sextabanda}`)
+
+ //Ejercicio 5 
+
+ let alumnas = []
+
+ alumnas[0] = 'Ada'
+ alumnas[1] = 'Grace'
+ alumnas[2] = 'Radia' 
+ alumnas[3] = 'Janie'
+ alumnas[4] = 'Sheryl'
+ alumnas[5] = 'Hedy'
+
+ console.log(`índice 0: ${alumnas[0]}`)
+ console.log(`índice 1: ${alumnas[1]}`)
+ console.log(`índice 2: ${alumnas[2]}`)
+ console.log(`índice 3: ${alumnas[3]}`)
+ console.log(`índice 4: ${alumnas[4]}`)
+ console.log(`índice 5: ${alumnas[5]}`)
+
+ //Ejercicio 
+
+ let ganadorasRupaul = ['BeBe Zahara Benet', 'Tyra Sanchez', 'Raja', 'Sharon Needles', 'Jinkx Monsoon', 'Bianca del Rio', 'Violet Chachki', 'Bob the Drag Queen', 'Sasha Velour', 'Aquaria'];
+
+console.log(`La primera ganadora es ${ganadorasRupaul[0]}`)
+
+console.log(`La ultima ganadora es: ${ganadorasRupaul[ganadorasRupaul.length - 1]}`)
+
+//Ejercicio 10
+
+ let playlistNirvana1 = ['Smells Like Teen Spirit', 'Come As You Are', 'Heart-Shaped Box', 'Lithium'];
+ let playlistFoo1 = ['Everlong', 'The Pretender', 'Learn to Fly'];
+
+ console.log(playlistNirvana1.length)
+ console.log(playlistFoo1.length)
+
+ console.log(`La playlist de Nirvana tiene más canciones es ${playlistNirvana1}`)
+
+
